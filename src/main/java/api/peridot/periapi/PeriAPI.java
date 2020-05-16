49959019481b9300340e76cf70621115956f09e5
Peridot <blackxxoj@gmail.com>
2020-05-16 22:17:47 +0200

    InventoryDragEvent

--- src/main/java/api/peridot/periapi/PeriAPI.java
+++ src/main/java/api/peridot/periapi/PeriAPI.java
@@ -3,6 +3,7 @@ package api.peridot.periapi;
 import api.peridot.periapi.inventories.PeriInventoryManager;
 import api.peridot.periapi.listeners.InventoryClickListener;
 import api.peridot.periapi.listeners.InventoryCloseListener;
+import api.peridot.periapi.listeners.InventoryDragListener;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
@@ -21,6 +22,7 @@ public class PeriAPI {
         PluginManager pluginManager = Bukkit.getPluginManager();
         pluginManager.registerEvents(new InventoryClickListener(plugin, this), plugin);
         pluginManager.registerEvents(new InventoryCloseListener(plugin, this), plugin);
+        pluginManager.registerEvents(new InventoryDragListener(plugin, this), plugin);
 
         inventoryManager = new PeriInventoryManager(plugin, this);
     }
