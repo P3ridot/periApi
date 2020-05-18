f5a4124c5280da43541a60e8e992f6562fe8cafa
Peridot <blackxxoj@gmail.com>
2020-05-18 15:38:24 +0200

    Debug

--- src/main/java/api/peridot/periapi/packets/NotificationPackets.java
+++ src/main/java/api/peridot/periapi/packets/NotificationPackets.java
@@ -11,14 +11,14 @@ public class NotificationPackets {
     private static Class<?> chatMessageTypeClass;
     private static Object chatMessageType;
 
-    private static Reflection.MethodInvoker getTitleActionEnum;
+    private static Reflection.MethodInvoker titleActionEnumMethod;
 
     private static Enum<?> titleEnum;
     private static Enum<?> subtitleEnum;
     private static Enum<?> timesEnum;
 
-    private static Reflection.ConstructorInvoker packetPlayOutTitle;
-    private static Reflection.ConstructorInvoker packetPlayOutChat;
+    private static Reflection.ConstructorInvoker packetPlayOutTitleConstructor;
+    private static Reflection.ConstructorInvoker packetPlayOutChatConstructor;
 
     static {
         try {
@@ -32,16 +32,16 @@ public class NotificationPackets {
             }
 
             titleActionClass = Reflection.serverVersion.equals("v1_8_R1") ? Reflection.getMinecraftClass("EnumTitleAction") : Reflection.getMinecraftClass("PacketPlayOutTitle$EnumTitleAction");
-            getTitleActionEnum = Reflection.getMethod(titleActionClass, "valueOf", String.class);
-            titleEnum = (Enum) getTitleActionEnum.invoke(titleActionClass, "TITLE");
-            subtitleEnum = (Enum) getTitleActionEnum.invoke(titleActionClass, "SUBTITLE");
-            timesEnum = (Enum) getTitleActionEnum.invoke(titleActionClass, "TIMES");
+            titleActionEnumMethod = Reflection.getMethod(titleActionClass, "valueOf", String.class);
+            titleEnum = (Enum) titleActionEnumMethod.invoke(titleActionClass, "TITLE");
+            subtitleEnum = (Enum) titleActionEnumMethod.invoke(titleActionClass, "SUBTITLE");
+            timesEnum = (Enum) titleActionEnumMethod.invoke(titleActionClass, "TIMES");
 
-            packetPlayOutTitle = Reflection.getConstructor(Reflection.getMinecraftClass("PacketPlayOutTitle"), titleActionClass, chatBaseComponentClass, int.class, int.class, int.class);
+            packetPlayOutTitleConstructor = Reflection.getConstructor(Reflection.getMinecraftClass("PacketPlayOutTitle"), titleActionClass, chatBaseComponentClass, int.class, int.class, int.class);
             if (chatMessageTypeClass == null) {
-                packetPlayOutChat = Reflection.getConstructor(Reflection.getMinecraftClass("PacketPlayOutChat"), chatBaseComponentClass, byte.class);
+                packetPlayOutChatConstructor = Reflection.getConstructor(Reflection.getMinecraftClass("PacketPlayOutChat"), chatBaseComponentClass, byte.class);
             } else {
-                packetPlayOutTitle = Reflection.getConstructor(Reflection.getMinecraftClass("PacketPlayOutChat"), chatBaseComponentClass, chatMessageTypeClass);
+                packetPlayOutChatConstructor = Reflection.getConstructor(Reflection.getMinecraftClass("PacketPlayOutChat"), chatBaseComponentClass, chatMessageTypeClass);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
@@ -51,9 +51,9 @@ public class NotificationPackets {
     public static List<Object> createTitlePacket(String title, String subTitle, int fadeIn, int stay, int fadeOut) {
         List<Object> packets = new ArrayList<>();
         try {
-            Object titlePacket = packetPlayOutTitle.invoke(titleEnum, CommonPackets.createBaseComponent(title), -1, -1, -1);
-            Object subtitlePacket = packetPlayOutTitle.invoke(subtitleEnum, CommonPackets.createBaseComponent(subTitle), -1, -1, -1);
-            Object timesPacket = packetPlayOutTitle.invoke(timesEnum, null, 10, 20, 20);
+            Object titlePacket = packetPlayOutTitleConstructor.invoke(titleEnum, CommonPackets.createBaseComponent(title), -1, -1, -1);
+            Object subtitlePacket = packetPlayOutTitleConstructor.invoke(subtitleEnum, CommonPackets.createBaseComponent(subTitle), -1, -1, -1);
+            Object timesPacket = packetPlayOutTitleConstructor.invoke(timesEnum, null, 10, 20, 20);
 
             packets.addAll(Arrays.asList(titlePacket, subtitlePacket, timesPacket));
         } catch (Exception ex) {
@@ -64,14 +64,10 @@ public class NotificationPackets {
 
     public static Object createActionBarPacket(String text) {
         Object packet = null;
-        try {
-            if (chatMessageTypeClass != null) {
-                packet = packetPlayOutChat.invoke(CommonPackets.createBaseComponent(text), chatMessageType);
-            } else {
-                packet = packetPlayOutChat.invoke(CommonPackets.createBaseComponent(text), (byte) 2);
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
+        if (chatMessageTypeClass != null) {
+            packet = packetPlayOutChatConstructor.invoke(CommonPackets.createBaseComponent(text), chatMessageType);
+        } else {
+            packet = packetPlayOutChatConstructor.invoke(CommonPackets.createBaseComponent(text), (byte) 2);
         }
         return packet;
     }
